158ea12020ca716da9709657056896cae2a337a2
xiphiasnonus <foster.pianist@gmail.com>
2019-02-14 13:10:48 -0500

    Initial Commit

---
+++ DungeonDave/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DungeonDave
+//
+//  Created by Thomas Foster on 2/14/19.
+//  Copyright © 2019 Thomas Foster. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
